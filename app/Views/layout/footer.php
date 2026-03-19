cdf3500cb0f5e3c207f4d3197a32cff8fe5a14b9
Shrestha <T.B.Shrestha2@wlv.ac.uk>
2026-03-19 16:45:17 +0000

    Fix: Move jQuery to header to resolve AJAX broken issue

--- app/Views/layout/footer.php
+++ app/Views/layout/footer.php
@@ -8,9 +8,10 @@
 </footer>
 
 <!-- jQuery MUST come before Bootstrap JS -->
-<script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
 <script src="<?= base_url('js/app.js') ?>"></script>
 </body>
 </html>
+
+
 
--- app/Views/layout/header.php
+++ app/Views/layout/header.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -7,6 +7,8 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="stylesheet" href="<?= base_url('css/style.css') ?>">
+    <!-- jQuery loaded in <head> so inline scripts in views can use $ immediately -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
 </head>
 <body>
 
